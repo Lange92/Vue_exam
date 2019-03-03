67f66c01df430e3d059cec9bfda8d3eee396e922
Lange92 <42997892+Lange92@users.noreply.github.com>
2019-03-04 00:04:55 +0100

    Register and Productview

--- src/views/Productview.vue
+++ src/views/Productview.vue
@@ -1,13 +1,273 @@
 <template>
-    
+  <div class="Product">
+    <div class="landing-page">
+      <img
+        src="../assets/imgs/product-landing-pic.png"
+        alt="Landing page picutre"
+        class="img-landing-page"
+      />
+    </div>
+    <div class="width-container">
+      <div class="Pcontainer">
+        <div>
+          <img src="../assets/imgs/productpic1.png" alt />
+        </div>
+        <div class="Pcontainer__elem">
+          <h1 class="Pcontainer__elem--h1">Ave classic sweatshirt</h1>
+          <div class="Pcontainer__elem--review">
+            <div>
+              <p>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i>
+                <i class="far fa-star"></i> 3 Review(s)
+              </p>
+            </div>
+            <div>
+              <p>|</p>
+            </div>
+            <div>
+              <p>Add a review</p>
+            </div>
+            <div>
+              <p>|</p>
+            </div>
+            <div>
+              <p>Share:</p>
+            </div>
+            <div>
+              <i class="fab fa-facebook-f"></i>
+            </div>
+            <div>
+              <i class="fab fa-twitter"></i>
+            </div>
+            <div>
+              <i class="fab fa-instagram"></i>
+            </div>
+            <div>
+              <i class="fab fa-pinterest"></i>
+            </div>
+          </div>
+          <div class="Pcontainer__elem">
+            <div class="Pcontainer__price">
+              <h3>
+                <span>£</span>
+              </h3>
+              <h3 class="Pcontainer__price--line">107</h3>
+              <h3>
+                <span>£</span>
+              </h3>
+              <h3>89.99</h3>
+            </div>
+          </div>
+          <div class="Pcontainer__elem">
+            <div class="Pcontainer__info">
+              <h4>
+                AVAILABILITY:
+                <span>In stock</span>
+              </h4>
+              <h4>
+                PRODUCT CODE:
+                <span>#499577</span>
+              </h4>
+              <h4>
+                TAGS:
+                <span class="Pcontainer__info--blue"
+                  >Classic, Casual, V-Neck, Loose</span
+                >
+              </h4>
+            </div>
+          </div>
+          <div class="Pcontainer__infotext">
+            <p>
+              Donec sem lorem laoreet tempor un risus vitae, rutrum sodales nibh
+              suspendisse est congue metus nunc, id viverra elit loreti rhoncus
+              quis consecteur es. Donec pulvinar tempor lorem a pretium justo
+              interdum.
+            </p>
+            <ul class="Pcontainer__infotext--circle">
+              <li>Elsaticated cuffs</li>
+              <li>Casual fit</li>
+              <li>100% Cotton</li>
+              <li>Free Shipping with 4 days delivery</li>
+            </ul>
+          </div>
+          <div class="Pcontainer__input">
+            <div>
+              <h3>Colour</h3>
+              <select name id="Pcontainer__input--option">
+                <option value="GDP">Select Colour</option>
+                <option value="DKK">Red</option>
+                <option value="EURO">Blue</option>
+                <option value="EURO">Green</option>
+              </select>
+            </div>
+            <div>
+              <h3>size</h3>
+              <select name id="Pcontainer__input--option">
+                <option value="GDP">Select Size</option>
+                <option value="DKK">S</option>
+                <option value="EURO">M</option>
+                <option value="EURO">L</option>
+              </select>
+            </div>
+            <div>
+              <h3>qty</h3>
+              <input type="number" name="quantity" min="1" max="9" />
+            </div>
+          </div>
+          <div class="Pcontainer__button">
+            <button>
+              <i class="fas fa-shopping-cart"></i>
+              <h3>Add to cart</h3>
+            </button>
+            <button>
+              <i class="far fa-heart"></i>
+              <h3>add to lookbook</h3>
+            </button>
+          </div>
+          <div class="Pcontainer__compare">
+            <i class="fas fa-search"></i>
+            <p>Add to compare</p>
+          </div>
+        </div>
+        <div id="tabs" class="container__details">
+          <div class="tabs">
+            <a
+              v-on:click="activetab = 1"
+              v-bind:class="[activetab === 1 ? 'active' : '']"
+              >Description</a
+            >
+            <a
+              v-on:click="activetab = 2"
+              v-bind:class="[activetab === 2 ? 'active' : '']"
+              >video</a
+            >
+            <a
+              v-on:click="activetab = 3"
+              v-bind:class="[activetab === 3 ? 'active' : '']"
+              >SIze &amp; Specs</a
+            >
+            <a
+              v-on:click="activetab = 4"
+              v-bind:class="[activetab === 4 ? 'active' : '']"
+              >Delivery &amp; returns</a
+            >
+            <a
+              v-on:click="activetab = 5"
+              v-bind:class="[activetab === 5 ? 'active' : '']"
+              >Reviews</a
+            >
+          </div>
+
+          <div class="content">
+            <div v-if="activetab === 1" class="tabcontent">
+              <h5>Nunc egestas posuere enim, eu maximus erat posuere eget</h5>
+              <p>
+                Sed ut mi mollis, consequat nulla lacinia, hendrerit turpis.
+                Nulla sapien magna, interdum quis pretium nec, pharetra at
+                felis. Curabitur dictum sapien est, eget ultricies turpis porta
+                vel. Nam suscipit nec lacus sed imperdiet. Vestibulum a purus
+                risus. Nulla et dictum augue, nec efficitur mi. Nam sit amet
+                pretium elit. Aliquam congue, ligula id vehicula vestibulum,
+                orci ex vulputate lacus, ac malesuada elit dolor eget ex. Sed
+                quis aliquet risus, ut cursus lectus. In eget lorem tellus.
+                Quisque eleifend varius nisi nec sagittis. Nulla ullamcorper
+                imperdiet justo, ut venenatis purus lobortis ut. Nunc sagittis
+                urna et hendrerit sodales. Nunc molestie risus nec fringilla
+                lacinia. Nulla facilisi. Etiam neque velit, tristique eget
+                sollicitudin eget, placerat at metus. Proin dictum lobortis
+                velit, id suscipit orci faucibus ut. Aliquam erat volutpat.
+                Vivamus feugiat justo in diam placerat, id dignissim elit
+                auctor. Vestibulum scelerisque sem et lobortis ultricies. Morbi
+                suscipit nulla urna. Suspendisse potenti. Nullam varius quam sed
+                nisl dignissim, vel faucibus ipsum blandit. Vivamus at suscipit
+                augue. Nam finibus gravida lorem eu viverra. Praesent rhoncus
+                imperdiet ultricies. Nullam pretium cursus augue auctor
+                vulputate. Quisque a convallis diam commodo eget diam id,
+                eleifend dictum libero. Etiam varius, nisi vel dignissim
+                sodales, enim dui posuere mauris, in aliquet lorem eros eget
+                neque.
+              </p>
+            </div>
+            <div v-if="activetab === 2" class="tabcontent">
+              <h5>Video</h5>
+              <p>
+                Sed ut mi mollis, consequat nulla lacinia, hendrerit turpis.
+                Nulla sapien magna, interdum quis pretium nec, pharetra at
+                felis. Curabitur dictum sapien est, eget ultricies turpis porta
+                vel. Nam suscipit nec lacus sed imperdiet. Vestibulum a purus
+                risus. Nulla et dictum augue, nec efficitur mi. Nam sit amet
+                pretium elit. Aliquam congue, ligula id vehicula vestibulum,
+                orci ex vulputate lacus, ac malesuada elit dolor eget ex. Sed
+                quis aliquet risus, ut cursus lectus. In eget lorem tellus.
+                Quisque eleifend varius nisi nec sagittis. Nulla ullamcorper
+                imperdiet justo, ut venenatis purus lobortis ut. Nunc sagittis
+                urna et hendrerit sodales. Nunc molestie risus nec fringilla
+              </p>
+            </div>
+            <div v-if="activetab === 3" class="tabcontent">
+              <h5>Here you can find sizes</h5>
+              <p>
+                Sed ut mi mollis, consequat nulla lacinia, hendrerit turpis.
+                Nulla sapien magna, interdum quis pretium nec, pharetra at
+                felis. Curabitur dictum sapien est, eget ultricies turpis porta
+                vel. Nam suscipit nec lacus sed imperdiet. Vestibulum a purus
+                risus. Nulla et dictum augue, nec efficitur mi. Nam sit amet
+                pretium elit. Aliquam congue, ligula id vehicula vestibulum,
+                orci ex vulputate lacus, ac malesuada elit dolor eget ex. Sed
+                quis aliquet risus, ut cursus lectus. In eget lorem tellus.
+                Quisque eleifend varius nisi nec sagittis. Nulla ullamcorper And
+                there is a lot more to find if you look
+              </p>
+            </div>
+            <div v-if="activetab === 4" class="tabcontent">
+              <h5>WE DONT DELIVER OR RETURN</h5>
+              <p>
+                Sed ut mi mollis, consequat nulla lacinia, hendrerit turpis.
+                Nulla sapien magna, interdum quis pretium nec, pharetra at
+                felis. Curabitur dictum sapien est, eget ultricies turpis porta
+                vel. Nam suscipit nec lacus sed imperdiet. Vestibulum a purus
+                risus. Nulla et dictum augue, nec efficitur mi. Nam sit amet
+                pretium elit. Aliquam congue, ligula id vehicula vestibulum,
+                orci ex vulputate lacus, ac malesuada elit dolor eget ex. Sed
+                quis aliquet risus, ut cursus lectus. In eget lorem tellus.
+                Quisque eleifend varius nisi nec sagittis. Nulla ullamcorper And
+                there is a lot more to find if you look
+              </p>
+            </div>
+          </div>
+          <div v-if="activetab === 5" class="tabcontent">
+            <h5>Our reviews are wonderful, you wouldnt believe it</h5>
+            <p>
+              Sed ut mi mollis, consequat nulla lacinia, hendrerit turpis. Nulla
+              sapien magna, interdum quis pretium nec, pharetra at felis.
+              Curabitur dictum sapien est, eget ultricies turpis porta vel. Nam
+              suscipit nec lacus sed imperdiet. Vestibulum a purus risus. Nulla
+              et dictum augue, nec efficitur mi. Nam sit amet pretium elit.
+              Aliquam congue, ligula id vehicula vestibulum, orci ex vulputate
+              lacus, ac malesuada elit dolor eget ex. Sed quis aliquet risus, ut
+              cursus lectus. In eget lorem tellus. Quisque eleifend varius nisi
+              nec sagittis. Nulla ullamcorper And there is a lot more to find if
+              you looklacus, ac malesuada elit dolor eget ex. Sed quis aliquet
+              risus, ut cursus lectus. In eget lorem tellus. Quisque eleifend
+              varius nisi nec sagittis. Nulla ullamcorper And there is a lot
+              more to find if you look
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data: function() {
+    return { activetab: 1 }
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -8,11 +8,129 @@
       />
     </div>
     <div class="width-container">
-      <div></div>
+      <form id="app" @submit="checkForm" action="/something" method="post">
+        <div class="RegContainer">
+          <div>
+            <h2>Sign in</h2>
+            <input
+              type="text"
+              name="email"
+              id="email"
+              v-model="email"
+              placeholder="Your Email..."
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="password"
+              id="password"
+              v-model="password"
+              placeholder="Your Password..."
+            />
+          </div>
+          <div class="RegContainer__input--button">
+            <button type="submit" v-on:click="checkForm()">Sign in</button>
+          </div>
+          <div>
+            <h2>Register</h2>
+            <input
+              type="text"
+              name="emailReg"
+              id="emailReg"
+              v-model="emailReg"
+              placeholder="Your Email.."
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="passwordReg"
+              id="passwordReg"
+              v-model="passwordReg"
+              placeholder="Your Password.."
+            />
+          </div>
+          <div>
+            <input
+              type="text"
+              name="confirmPasswordReg"
+              id="confirmPasswordReg"
+              v-model="confirmPasswordReg"
+              placeholder="Confirm Password.."
+            />
+          </div>
+          <div class="RegContainer__input--align">
+            <form action="/action_page.php">
+              <input type="checkbox" name="vehicle1" value="Bike" />
+              <h4>
+                Sign up for exlusive updates, discounts new arrivals, contests
+                and more!
+              </h4>
+            </form>
+          </div>
+          <div class="RegContainer__input--buttonTwo">
+            <button type="submit" v-on:click="checkForm2()">
+              Create Account
+            </button>
+          </div>
+          <div class="RegContainer__input--p">
+            <p>
+              Forgot your password
+              <i class="fas fa-long-arrow-alt-right"></i>
+            </p>
+          </div>
+          <div>
+            <p>
+              By clicking ‘Create Account’, you agree to our
+              <span>
+                Privacy Policy
+                <i class="fas fa-long-arrow-alt-right"></i>
+              </span>
+            </p>
+          </div>
+          <div v-if="errors.length" class="RegContainer__error">
+            <b>Please correct the following error(s):</b>
+            <ul>
+              <li v-for="error in errors" :key="error">{{ error }}</li>
+              <li v-for="error in errorsTwo" :key="error">{{ error }}</li>
+            </ul>
+          </div>
+        </div>
+      </form>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data: function() {
+    return {
+      errors: [],
+      email: null,
+      password: null,
+      emailReg: null,
+      passwordReg: null,
+      confirmPasswordReg: null
+    }
+  },
+  methods: {
+    checkForm: function(e) {
+      if (this.email && this.password) return true
+      this.errors = []
+      if (!this.email) this.errors.push('Email required.')
+      if (!this.password) this.errors.push('Password Required')
+      e.preventDefault()
+    },
+    checkForm2: function(e) {
+      if (this.emailReg && this.passwordReg && this.confirmPasswordReg)
+        return true
+      this.errorsTwo = []
+      if (!this.emailReg) this.errorsTwo.push('Email required.')
+      if (!this.passwordReg) this.errorsTwo.push('Password Required')
+      if (!this.confirmPasswordReg) this.errorsTwo.push('Confirmation needed')
+      e.preventDefault()
+    }
+  }
+}
 </script>
